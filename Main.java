4c362ef6b234063e9e4c8521412afb78e32a379c
Edwin Sanchez <edwinsanchez2003@gmail.com>
2023-06-12 18:45:46 -0400

    init repository, added Main.java & Main2.java, added .gitignore file

---
+++ Main.java
@@ -0,0 +1,25 @@
+/*
+ * Demo Code - Hello World
+ * Edwin Sanchez
+ * 
+ * HOW TO RUN:
+ * 1) Write Java Code
+ * 2) To compile the code, run: 
+ *    - javac FILE_NAME.java
+ * 3) If the previous command compiles without errors, run: 
+ *    - java FILE_NAME
+ */
+
+class Main {
+  
+  // main function - where our program starts
+  public static void main(String[] args) {
+    helloWorld(); // call helloWorld() function
+  } // end main function
+
+
+  // Prints 'Hello World!' to the console
+  public static void helloWorld() {
+    System.out.println("Hello World!");
+  }; // end helloWorld
+} // end Main class
---
+++ Main2.java
@@ -0,0 +1,25 @@
+/*
+ * Practice Java
+ * Edwin Sanchez
+ * 
+ * Brief description of the goals of this code file.
+ */
+
+// This is another comment
+
+
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("Hello, Edwin!");
+        myFunction();
+        myFunction();
+        myFunction();
+    } // end main
+
+
+    public static void myFunction() {
+        System.out.println("This is my first message!");
+        System.out.println("This is my second message!");
+        System.out.println("This is my third message!");
+    } // end myFunction
+} // end Main2 class
